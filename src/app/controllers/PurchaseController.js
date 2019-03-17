da67f40829346663bcc361710ed8a2134e5978d1
joaaoeu <hello@joaao.eu>
2019-03-17 03:41:07 -0300

    Added handlebars + purchase email view + update Insomnia docs

--- src/app/controllers/PurchaseController.js
+++ src/app/controllers/PurchaseController.js
@@ -7,13 +7,14 @@ class PurchaseController {
     const { ad, message } = req.body
 
     const purchaseAd = await Ad.findById(ad).populate('author')
-    // const user = await User.findById(req.userId)
+    const user = await User.findById(req.userId)
 
     await Mail.sendMail({
       from: '"Marketplace" <no-reply@marketplace.com>',
       to: purchaseAd.author.email,
       subject: `Solicitação de compra: ${purchaseAd.title}`,
-      html: `<p>${message}</p>`
+      template: 'purchase',
+      context: { ad: purchaseAd, user, message }
     })
 
     return res.status(204).send()
--- src/app/services/Mail.js
+++ src/app/services/Mail.js
@@ -1,6 +1,20 @@
 const nodemailer = require('nodemailer')
+const path = require('path')
+const hbs = require('nodemailer-express-handlebars')
 const mailConfig = require('../../config/mail')
 
 const transport = nodemailer.createTransport(mailConfig)
+
+transport.use(
+  'compile',
+  hbs({
+    viewEngine: {
+      extName: '.hbs',
+      partialsDir: '.'
+    },
+    viewPath: path.resolve(__dirname, '..', 'views', 'emails'),
+    extName: '.hbs'
+  })
+)
 
 module.exports = transport
