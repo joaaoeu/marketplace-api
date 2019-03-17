6b58af649048a5ec84744d5630b5c049e76fb1eb
joaaoeu <hello@joaao.eu>
2019-03-16 23:54:20 -0300

    Added AdController + routes GET, POST, PUT and DELETE /ads + update Insomnia docs

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,24 @@
 const express = require('express')
+const middlewares = require('./app/middlewares')
 const controllers = require('./app/controllers')
 
 const routes = express.Router()
 
+/** Public routes **/
+/** Auth **/
 routes.post('/auth', controllers.AuthController.store)
+
+/** Users **/
 routes.post('/users', controllers.UserController.store)
+
+/** Private routes **/
+routes.use(middlewares.auth)
+
+/** Ads **/
+routes.get('/ads', controllers.AdController.index)
+routes.get('/ads/:id', controllers.AdController.show)
+routes.post('/ads', controllers.AdController.store)
+routes.put('/ads/:id', controllers.AdController.update)
+routes.delete('/ads/:id', controllers.AdController.destroy)
 
 module.exports = routes
