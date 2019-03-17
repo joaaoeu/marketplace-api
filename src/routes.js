67cf84bfdf830e36cf45dd513b487e408dc3c856
joaaoeu <hello@joaao.eu>
2019-03-16 22:31:16 -0300

    Fix npm run lint + fix routes lint problem

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,6 @@ const express = require('express')
 
 const routes = express.Router()
 
-const authMiddleware = require('./app/middlewares/auth')
-
 const AuthController = require('./app/controllers/AuthController')
 const UserController = require('./app/controllers/UserController')
 
